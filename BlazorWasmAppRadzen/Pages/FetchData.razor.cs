dc81231777f5df5bead5eb258cb900278d25eb24
Atanas Korchev <akorchev@gmail.com>
2022-12-29 20:13:18 +0200

    Set height to enable virtualization. Collapse groups by default.

--- BlazorWasmAppRadzen/Pages/FetchData.razor.cs
+++ BlazorWasmAppRadzen/Pages/FetchData.razor.cs
@@ -1,14 +1,34 @@
 using CSharpSharedData;
+using Radzen;
 
 namespace BlazorWasmAppRadzen.Pages
 {
-   public partial class FetchData
-   {
-      private WeatherForecast[]? forecasts;
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+        private HashSet<object> expanded = new ();
 
-      protected override void OnInitialized()
-      {
-         forecasts = WeatherForecast.Data;
-      }
-   }
+        protected override void OnInitialized()
+        {
+            forecasts = WeatherForecast.Data;
+        }
+
+        void OnGroupRowRender(GroupRowRenderEventArgs args)
+        {
+            if (!expanded.Contains(args.Group.Data.Key))
+            {
+                args.Expanded = false;
+            }
+        }
+
+        void OnGroupRowExpand(Group args)
+        {
+            expanded.Add(args.Data.Key);
+        }
+
+        void OnGroupRowCollapse(Group args)
+        {
+            expanded.Remove(args.Data.Key);
+        }
+    }
 }
